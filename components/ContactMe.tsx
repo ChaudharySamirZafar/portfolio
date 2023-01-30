9cdc5a35cf08108a464831972546702f6f050100
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2023-01-30 14:12:34 +0000

    'Fixed Contact Me Page'

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,12 +24,12 @@ export default function ContactMe({}: Props) {
       </h3>
 
       <div className="flex flex-col space-y-4 md:space-y-5 lg:space-y-6 xl:space-y-6 2xl:space-y-10">
-        <h4 className="text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-semibold text-center">
+        <h4 className="text-sm md:text-2xl lg:text-3xl 2xl:text-4xl font-semibold text-center">
           I have got just what you need.{" "}
           <span className="decoration-[#F7AB0A]  underline">Lets talk.</span>
         </h4>
 
-        <div className="space-y-1 md:space-y-3 lg:space-y-3 xl:space-y-3 2xl:space-y-5">
+        <div className="space-y-1 md:space-y-3 lg:space-y-3 xl:space-y-3 2xl:space-y-5 hidden md:block">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
             <p className="text-lg md:text-2xl lg:text-2xl">+44 7456979575</p>
