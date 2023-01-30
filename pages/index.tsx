9e20831e06d5e97f16dfbe20530213050344aa30
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2023-01-30 21:58:16 +0000

    'Updated website icon'

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Samirs Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <section id="hero" className="snap-center">
