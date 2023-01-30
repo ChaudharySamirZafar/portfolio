384b38d3afd11fd091a8a20196c42173de0e42d5
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2023-01-30 13:21:24 +0000

    'Aboout Section Fixed'

--- components/About.tsx
+++ components/About.tsx
@@ -9,11 +9,11 @@ function About({}: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-xl md:text-2xl">
         About
       </h3>
       <motion.img
@@ -25,19 +25,20 @@ function About({}: Props) {
           duration: 1.2,
         }}
         whileInView={{
-          opacity: 1,
           x: 0,
+          opacity: 1,
         }}
+        viewport={{ once: true }}
+        className=" -mb-24 md:mb-0 flex-shrink-0 w-40 h-46 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
         src="https://drive.google.com/uc?export=view&id=1xCAeMp118wGAe-TZ2rjV0ag9VXnxTIYX"
-        className="mt-10 md:mt-0 md:mb-0 flex-shrink-0 w-38 h-48 rounded-full object cover md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[600px]"
       />
-      <div className="space-y-2 px-2 md:px-10">
-        <h4 className="text-2xl font-semibold  lg:text-4xl">
+      <div className="space-y-5 md:space-y-10 px-0 md:px-10">
+        <h4 className="text-xl md:text-4xl font-semibold">
           Here is a{" "}
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background
         </h4>
-        <p className="text-sm lg:text-lg">
+        <p className="text-sm md:text-lg lg:text-lg text-justify">
           I am Samir Zafar, a computer science student in my final year of
           university. I possess a strong passion for creating innovative and
           impactful software solutions. I have gained valuable experience in
